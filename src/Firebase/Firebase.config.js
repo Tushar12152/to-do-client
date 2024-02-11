d05d562cab5d4c154d80e40fb84770e4177bcf80
Tushar12152 <tusharimran470@gmail.com>
2024-02-11 18:22:57 +0600

    firebase config changed

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,3 +1,5 @@
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
@@ -6,15 +8,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyADkYFOb5ulSMXpK180_sz853CpWzA42Uw",
-  authDomain: "wedding-event-management-f2d82.firebaseapp.com",
-  projectId: "wedding-event-management-f2d82",
-  storageBucket: "wedding-event-management-f2d82.appspot.com",
-  messagingSenderId: "869193857109",
-  appId: "1:869193857109:web:cdeb7a08a773d11cb13cdc"
+  apiKey: "AIzaSyB_SPPWuzW2AnTLl6OUFCeNKIA1FdmZ6Nk",
+  authDomain: "to-do-app-5e494.firebaseapp.com",
+  projectId: "to-do-app-5e494",
+  storageBucket: "to-do-app-5e494.appspot.com",
+  messagingSenderId: "829911367259",
+  appId: "1:829911367259:web:e82be3c8e389a44cb8097e"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth=getAuth(app)
 export default auth;
